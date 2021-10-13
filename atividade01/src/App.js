5e1c58adb591199d9bf05455db2ba14b8cd5bd48
ruggerygusmao <ruggery402@gmail.com>
2021-10-12 22:04:10 -0300

    atividade componentes-react

---
+++ atividade01/src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { Mensagem } from "./Mensagem.js";
+
+export default function App(){
+  return(
+  <Mensagem />
+  );
+}
---
+++ atividade01/src/Mensagem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Mensagem = () => (
+    <div>
+        <h1>Trabalho mais disciplina é igual a êxito.</h1>
+    </div>
+);
